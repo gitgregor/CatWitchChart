ee97ad347f4f4ab24d976e6c76bbadd3b4388d63
Gregory <gerda.trust@gmail.com>
2020-10-26 16:27:28 +0100

    Set a flexbox property to Header
    containers areadded for tags

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Route, Link, Switch, Redirect } from 'react-router-dom'
+import Header from '../Components/Header/Header'
 import User from './UserPage'
 import Admin from './AdminPage'
+import Logo from '../Assets/Logo.svg';
 
 import './styles/mainFlexContainer.css'
 
@@ -10,11 +12,29 @@ const HomePage = ({ changeColorToggler, DarkModeClickHandler }) => {
         <div>
 
             <div class="wrapper">
-                <header class="header">
-                    <h2>Home page</h2>
-                    Header
-                    <button onClick={DarkModeClickHandler}>{changeColorToggler === false ? 'Day Mode' : 'Dark Mode'}</button>
-                </header>
+                <Header className="header" >
+                    <div className="header-wrapper">
+
+                        <div className="logo">
+                            <div>
+                                <img src={Logo} className="App-logo" alt="logo" />
+                            </div>
+                            <div>
+                                <h2>Home page</h2>
+                            </div>
+                        </div>
+
+                        <div className="dm-button">
+                            <button
+                                onClick={DarkModeClickHandler}
+                            >
+                                {changeColorToggler === false ? 'Dark Mode' : 'Day Mode'}
+                            </button>
+                        </div>
+
+                    </div>
+                </Header>
+
                 <Route path="/mainlayoutUser"  >
                     <article class="main">
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
