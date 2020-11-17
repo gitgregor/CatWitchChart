e68f466681b631d8c0dac4fb7f3dc0c166079754
Gregory <gerda.trust@gmail.com>
2020-11-18 00:02:24 +0100

    Changed data object

--- src/Components/OrgChart/OrgChart.js
+++ src/Components/OrgChart/OrgChart.js
@@ -2,27 +2,67 @@ import React from "react";
 // import styled from "@emotion/styled";
 import styled from 'styled-components'
 import { Tree, TreeNode } from "react-organizational-chart";
+import { object } from "prop-types";
 
 const StyledNode = styled.div`
   padding: 5px;
   border-radius: 8px;
   display: inline-block;
-  border: 1px solid gray;
-   /* background-color: #5b723a;
-   opacity: 0.7; */
-  /* border: 1px solid gray;
-  background-color: #5b723a;
-  color: white; */
+  /* border: 1px solid gray; */
+  /* background-color: #5b723a;
+ opacity: 0.7; */
+ border: 1px solid gray;
+background-color: #5b723a;
+color: white; 
 `;
 
 
-
+const data = {
+    L0: "warszawa",
+    L11: "katowice",
+    L21: "sosnowiec",
+    L22: "chorzów",
+    L12: "gdańsk",
+    L23: "elbląg",
+    L24: "pruszcz",
+    L13: "wrocław",
+    L25: "nagodzice",
+    L26: "pszczyna",
+}
 
 const StyledTreeExample = () => {
+
+    const { L0, L11, L12, L13, L21, L22, L23, L24, L25, L26 } = data
+
 
     return (
         <>
             <Tree
+                lineWidth={"2px"}
+                lineColor={"green"}
+                lineBorderRadius={"10px"}
+                label={<StyledNode>{L0}</StyledNode>}>
+                <TreeNode label={<StyledNode>{L11}</StyledNode>}>
+                    <TreeNode label={<StyledNode>{L21}</StyledNode>}></TreeNode>
+                    <TreeNode label={<StyledNode>{L22}</StyledNode>}></TreeNode>
+                </TreeNode>
+                <TreeNode label={<StyledNode>{L12}</StyledNode>}>
+                    <TreeNode label={<StyledNode>{L23}</StyledNode>}></TreeNode>
+                    <TreeNode label={<StyledNode>{L24}</StyledNode>}></TreeNode>
+                </TreeNode>
+                <TreeNode label={<StyledNode>{L13}</StyledNode>}>
+                    <TreeNode label={<StyledNode>{L25}</StyledNode>}></TreeNode>
+                    <TreeNode label={<StyledNode>{L26}</StyledNode>}></TreeNode>
+                </TreeNode>
+            </Tree>
+        </>
+    )
+}
+
+export default StyledTreeExample;
+
+
+{/* <Tree
                 lineWidth={"2px"}
                 lineColor={"green"}
                 lineBorderRadius={"10px"}
@@ -41,9 +81,11 @@ const StyledTreeExample = () => {
                     <TreeNode label={<StyledNode>Poznań L2/P</StyledNode>} />
                     <TreeNode label={<StyledNode>Gniezno L2/Gn</StyledNode>} />
                 </TreeNode>
-            </Tree>
+            </Tree> */}
 
-            {/* <Tree
+
+
+{/* <Tree
                 lineWidth={"2px"}
                 lineColor={"green"}
                 lineBorderRadius={"10px"}
@@ -63,8 +105,10 @@ const StyledTreeExample = () => {
                     <TreeNode label={<StyledNode>Grand Child 2</StyledNode>} />
                 </TreeNode>
             </Tree> */}
-        </>
-    );
-}
 
-export default StyledTreeExample;
+
+/* background-color: #5b723a;
+ opacity: 0.7; */
+/* border: 1px solid gray;
+background-color: #5b723a;
+color: white; */
